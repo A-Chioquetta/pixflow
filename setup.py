f443405be1c7d6ab402ea77c882d7d3f2189faf9
Alessandra <alessandra.chioquetta@gmail.com>
2025-05-26 22:53:56 -0300

    Add GitHub Actions workflow for PyPI publishing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pixflow",
-    version="0.1.0",
+    version="0.1.1",
     author="Alessandra Chioquetta",
     description="A simple image processing toolkit for resizing, histogram transfer, and difference detection.",
     long_description=long_description,
